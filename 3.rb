1fa4bb436b2ac9af663fcb18a1cafe35af49c367
Janice Wong <go.janicewong@gmail.com>
2017-12-16 18:01:13 -0500

    Add method to find a num 's factors - to be refactored

--- 3.rb
+++ 3.rb
@@ -1,6 +1,6 @@
 def is_prime(num)
   # a number is prime if its only factors are are one and itself
-  return false if num % 2 == 0
+  return false if num.even?
   (3..(num / 2)).each do |i|
     return false if num % i == 0
   end
@@ -11,4 +11,39 @@ end
 #   p "is #{number} prime? #{is_prime(number)}"
 # end
 
-def
+def factors_of_non_primes(num)
+  factors_array = []
+  if num.odd?
+    (3..(num / 2).to_i).step(2).each do |i|
+      # if divisible
+      if num % i == 0
+        return factors_array if factors_array.index(i)
+        divisor = i
+        quotient = num / i
+        if divisor != quotient
+          factors_array << divisor
+          factors_array << quotient
+        else
+          factors_array << divisor
+          return factors_array
+        end
+      end
+    end
+  else
+    (2..(num / 2).to_i).each do |i|
+      # if divisible
+      if num % i == 0
+        return factors_array if factors_array.index(i)
+        divisor = i
+        quotient = num / i
+        if divisor != quotient
+          factors_array << divisor
+          factors_array << quotient
+        else
+          factors_array << divisor
+          return factors_array
+        end
+      end
+    end
+  end
+end
